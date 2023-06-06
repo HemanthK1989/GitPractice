c7c8576f0c4d962272a9f44fc63b54cc9a5cc2b1
Hemanth <hemanthkrishnarao1989@gmail.com>
2023-06-06 15:40:59 +0530

    Git Practice Guy Branch commit

---
+++ Framework/src/test/java/HemanthAcademy/GitPracticeDevBranch.java
@@ -0,0 +1,10 @@
+package HemanthAcademy;
+
+import org.testng.annotations.Test;
+
+public class GitPracticeDevBranch {
+	@Test
+	public void test() {
+		System.out.println("GitPractice guy method");
+	}
+}
