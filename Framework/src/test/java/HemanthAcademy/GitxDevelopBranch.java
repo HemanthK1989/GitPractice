920b4d7b7a20875c7d1d02de9f8fd95738e1f5cc
Hemanth <hemanthkrishnarao1989@gmail.com>
2023-06-06 15:22:19 +0530

    Gitx guy commit in develop branch

---
+++ Framework/src/test/java/HemanthAcademy/GitxDevelopBranch.java
@@ -0,0 +1,12 @@
+package HemanthAcademy;
+
+import org.testng.annotations.Test;
+
+public class GitxDevelopBranch {
+	@Test
+	public void Gitxmethod_BranchDevelop () {
+		
+		System.out.println("Gitx guy created this method");
+	}
+
+}
